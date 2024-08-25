59c9379187f74f524114a95852a9896afc175f2c
zalesiu <zalewmat31@gmail.com>
2024-08-26 00:05:14 +0200

    feat: add alt property to backgrounds array

--- src/utils.ts
+++ src/utils.ts
@@ -6,10 +6,10 @@ import social from "./assets/icon-social.svg";
 import study from "./assets/icon-study.svg";
 
 export const backgrounds = [
-  { color: "bg-soft-orange", image: work },
-  { color: "bg-soft-blue", image: play },
-  { color: "bg-light-red", image: study },
-  { color: "bg-lime-green", image: exercise },
-  { color: "bg-violet", image: social },
-  { color: "bg-yellow", image: selfCare },
+  { color: "bg-soft-orange", image: work, alt: "briefcase" },
+  { color: "bg-soft-blue", image: play, alt: "gamepad icon" },
+  { color: "bg-light-red", image: study, alt: "open book" },
+  { color: "bg-lime-green", image: exercise, alt: "running person icon" },
+  { color: "bg-violet", image: social, alt: "speech bubble with three dots" },
+  { color: "bg-yellow", image: selfCare, alt: "heart with plus inside of it" },
 ];
